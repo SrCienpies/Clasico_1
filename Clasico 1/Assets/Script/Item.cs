180f978152953012b60b016cc179f2fbdd182a20
SrCienpies <53209785+SrCienpies@users.noreply.github.com>
2020-01-15 12:42:34 -0500

    Revert "Level Edits"
    This reverts commit d7cb4748eb91ddcafd2a8be4865b4afcaf37bcfd.

---
+++ Clasico 1/Assets/Script/Item.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    public CircleCollider2D colision;
+    public GameObject icono; 
+    public GameObject particulas;
+
+    public void Start()
+    {
+
+    }
+
+    public void Desvanecer()
+    {
+        colision.enabled = false;
+        icono.SetActive(false);
+        //particulas.SetActive(true);
+    }
+
+    public void Restablecer()
+    {
+        colision.enabled = true;
+        icono.SetActive(true);
+    }
+}
